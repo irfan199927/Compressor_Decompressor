bd37406627ef99621726c444b6c150e63bf8ea62
irfan199927 <71958886+irfan199927@users.noreply.github.com>
2023-06-20 23:50:27 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,30 +1,5 @@
-# Compressor_Decompressor
+#Compressor_Decompressor
 
-The provided code is a Java class named Decompressor that contains a method decompressFile() for decompressing a GZIP-compressed file. This code is intended to be part of a larger project related to file compression and decompression.
-
-Here's a summary of the code:
-
-The Decompressor class defines a single static method decompressFile() that takes a File object as a parameter. The method throws an IOException if there's an error during the decompression process.
-
-The method retrieves the parent directory of the input file using file.getParent() to determine where the decompressed file will be stored.
-
-It creates a FileInputStream to read the contents of the compressed file (file), and a GZIPInputStream to decompress the data from the input stream.
-
-A FileOutputStream is created to write the decompressed data to a new file named "DecompressedFile" in the same directory as the original compressed file.
-
-The method reads the compressed data from the GZIPInputStream in chunks (buffer) and writes the decompressed data to the output stream (fos).
-
-Once the decompression is complete, the method closes the GZIPInputStream, FileOutputStream, and FileInputStream to release the resources.
-
-This code provides a basic implementation for decompressing GZIP-compressed files in Java. However, it is important to note that this class alone does not constitute a complete project. It is likely that this class would be used in conjunction with other classes and functionality to create a comprehensive file compression and decompression system.
-
-
-
-
-
-User
-give summary including both compressor and decompressor as a project summary
-ChatGPT
 The provided code consists of two Java classes: Compressor and Decompressor. Together, they form a basic file compression and decompression system. Here's a summary of the project:
 
 Compressor class:
@@ -32,6 +7,7 @@ Compressor class:
 Contains a method compressFile() that takes a File object as input and compresses it using GZIP compression.
 It reads the contents of the input file using FileInputStream and writes the compressed data to a new file with the ".gz" extension using GZIPOutputStream.
 The compressed file is stored in the same directory as the original file.
+
 Decompressor class:
 
 Contains a method decompressFile() that takes a File object as input and decompresses it from GZIP format.
